6aefedf3dc26552bb65c7e632006c99234b381a0
Jay Weisskopf <jay@jayschwa.net>
2012-07-11 03:49:48 -0500

    Add a couple tests.

--- pty_test.go
+++ pty_test.go
@@ -2,6 +2,7 @@
 
 package pty
 
+import "bufio"
 import "fmt"
 import "io"
 import "os"
@@ -10,38 +11,145 @@ import "strings"
 import "testing"
 import "time"
 
+// Verify Open returns sane values.
 func TestOpen(t *testing.T) {
-	master, slave, err := Open()
+
+	master, slaveName, err := Open()
 	if err != nil {
-		t.Error(err)
+		t.Error("Open returned an error")
+		t.Fatal(err)
 	}
 	if master == nil {
-		t.Error("No master device")
+		t.Error("Open returned a nil master device")
+	} else {
+		defer master.Close()
 	}
-	t.Logf("Slave device path: '%s'\n", slave)
-	if !strings.HasPrefix(slave, "/dev/") {
-		t.Fail()
+	if slaveName == "" {
+		t.Error("Open returned an empty slave device path")
+	} else {
+		fmt.Printf("Slave device path: '%s'\n", slaveName)
+		tty, err := OpenTTY(slaveName)
+		if err != nil {
+			t.Error("OpenTTY returned an error")
+			t.Fatal(err)
+		}
+		if tty == nil {
+			t.Error("OpenTTY returned a nil slave device")
+		} else {
+			defer tty.Close()
+		}
 	}
 }
 
-func TestFork(t *testing.T) {
-	cmd := exec.Command("sh")
-	ptm, err := SetCmdTTY(cmd, "")
-	if err != nil {
-		t.Fatal(err)
+// Verify OpenTTY returns an error and nil when given bad paths.
+func TestOpenTTY_BadPaths(t *testing.T) {
+
+	tty, err := OpenTTY("")
+	if err == nil {
+		t.Error("OpenTTY did not return an error when given an empty path")
 	}
-	cmd.Start()
-	if err != nil {
-		t.Fatal(err)
+	if tty != nil {
+		t.Error("OpenTTY returned a non-nil device when given an empty path")
 	}
-	go io.Copy(os.Stdout, ptm)
-	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(ptm, "ps T")
-	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(ptm, "tty")
-	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(ptm, "who")
-	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(ptm, "exit")
-	cmd.Wait()
+
+	tty, err = OpenTTY("/some/bull/shit/path")
+	if err == nil {
+		t.Error("OpenTTY did not return an error when given a fake path")
+	}
+	if tty != nil {
+		t.Error("OpenTTY returned a non-nil device when given a fake path")
+	}
+
+	tty, err = OpenTTY("/bin/cat")
+	if err == nil {
+		t.Error("OpenTTY did not return an error when given a bad path")
+	}
+	if tty != nil {
+		t.Error("OpenTTY returned a non-nil device when given a bad path")
+	}
 }
+
+// Test data flow between a couple pseudoterminal device pairs.
+func TestPty_DataFlow(t *testing.T) {
+
+}
+
+// Verify that concurrent commands have different terminal devices.
+func TestSetCmdTTY_DiffDevs(t *testing.T) {
+
+	thisTTY := "FIXME"
+
+	thatCmd := exec.Command("tty")
+	otherCmd := exec.Command("tty")
+	thatMaster, err := SetCmdTTY(thatCmd, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if thatMaster == nil {
+		t.Fatal("SetCmdTTY returned a nil master device")
+	} else {
+		defer thatMaster.Close()
+	}
+	otherMaster, err := SetCmdTTY(otherCmd, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if otherMaster == nil {
+		t.Fatal("SetCmdTTY returned a nil master device")
+	} else {
+		defer otherMaster.Close()
+	}
+	err = thatCmd.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = otherCmd.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = thatCmd.Wait()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = otherCmd.Wait()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	thatTTY, err := bufio.NewReader(thatMaster).ReadString(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	thatTTY = strings.TrimSpace(thatTTY)
+	if thatTTY == "" {
+		t.Error("That TTY name is empty")
+	} else {
+		fmt.Printf("That TTY: '%s'\n", thatTTY)
+	}
+	otherTTY, err := bufio.NewReader(otherMaster).ReadString(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	otherTTY = strings.TrimSpace(otherTTY)
+	if otherTTY == "" {
+		t.Error("Other TTY name is empty")
+	} else {
+		fmt.Printf("Other TTY: '%s'\n", otherTTY)
+	}
+
+	if thisTTY == thatTTY {
+		t.Error("This TTY and That TTY are the same")
+	}
+	if thisTTY == otherTTY {
+		t.Error("This TTY and the Other TTY are the same")
+	}
+	if thatTTY == otherTTY {
+		t.Error("That TTY and the Other TTY are the same")
+	}
+}
+
+// Verify that the slave device File is closed after a command has been started.
+
+// Verify that SetCmdTTY returns an error when given bad paths.
+// Additionally, verify that TTY allocation "leaks" do not occur.
+
